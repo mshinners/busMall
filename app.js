760b4cbda382945be27d8a4cc366cb8042e572b1
Michael Shinners <mishinners@icloud.com>
2017-08-22 15:50:51 -0700

    Clicks clicking, list populating

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 'use strict';
-console.log('wat');
 //ImageDisplayField Constructor Function
 function Product(productName, imageFilePath, productId) {
   this.productName = productName;
@@ -13,12 +12,13 @@ function Product(productName, imageFilePath, productId) {
 //Global variables
 var productArray = [];
 var lastThreeImgs = [];
-var maxClicks = 25;
+var maxClicks = 3;
+var voteCounter = 0;
 
 //newly created arrays replacing code block immediately below
-var productName = ['bag', 'banbana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wine-Glass'];
+var productName = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wine-Glass'];
 var imageFilePath = ['imgs/bag.jpg', 'imgs/banana.jpg', 'imgs/bathroom.jpg', 'imgs/boots.jpg', 'imgs/breakfast.jpg', 'imgs/bubblegum.jpg', 'imgs/chair.jpg', 'imgs/cthulhu.jpg', 'imgs/dog-duck.jpg', 'imgs/dragon.jpg', 'imgs/pen.jpg', 'imgs/scissors.jpg', 'imgs/shark.jpg', 'imgs/sweep.png', 'imgs/tauntaun.jpg', 'imgs/unicorn.jpg', 'imgs/usb.gif', 'imgs/water-can.jpg', 'imgs/wine-glass.jpg', ];
-var productId = ['bag', 'banbana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wine-Glass'];
+var productId = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'waterCan', 'wine-Glass'];
 
 //running the following list into a for loop for all products
 for (var i = 0; i < imageFilePath.length; i++) {
@@ -77,55 +77,36 @@ renderThreeProducts();
 /////////////////////////////WORKING CODE ABOVE HERE////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-var imageOneViewed = document.getElementById('imageSpotOne');
-imageOneViewed.addEventListener('click', clickedTally);
-var imageTwoViewed = document.getElementById('imageSpotTwo');
-imageTwoViewed.addEventListener('click', clickedTally);
-var imageThreeViewed = document.getElementById('imageSpotThree');
-imageThreeViewed.addEventListener('click', clickedTally);
+var imageOneClicked = document.getElementById('imageSpotOne');
+imageOneClicked.addEventListener('click', clickedTally);
+var imageTwoClicked = document.getElementById('imageSpotTwo');
+imageTwoClicked.addEventListener('click', clickedTally);
+var imageThreeClicked = document.getElementById('imageSpotThree');
+imageThreeClicked.addEventListener('click', clickedTally);
 
 function clickedTally(event) {
-  var voteCounter = 0;
-  for (var i = 0; i < productArray.length; i++) {
-    if (productArray[i].productId === event.target.productId && voteCounter < maxClicks) {
-      productArray[i].votes++;
-      voteCounter++;
-      renderThreeProducts();
-    } else if (voteCounter === maxClicks){
-      oneClick.removeEventListener('click', vote, true);
-      twoClick.removeEventListener('click', vote, true);
-      threeClick.removeEventListener('click', vote, true);
-          // document.createElement
-      //  then create list and append
-      result.appendChild(theList);
-      for (var j = 0; j < productArray.length; i++) {
-        var list = document.createElement ('li');
-        list.innerText = productArray[j].votes / productArray[j].displayed * 100 + (' votes for the  ' + productArray[j].name + '.');
+  if (voteCounter < maxClicks) {
+    for (var i = 0; i < productArray.length; i++) {
+      if (productArray[i].productId == event.target.productId) {
+        productArray[i].votes++;
+        voteCounter++;
+        console.log(voteCounter);
+        console.log(productArray[i]);
+        renderThreeProducts();
       }
     }
   }
+  if (voteCounter === maxClicks) {
+    imageOneClicked.removeEventListener('click', clickedTally);
+    imageTwoClicked.removeEventListener('click', clickedTally);
+    imageThreeClicked.removeEventListener('click', clickedTally);
+    var unList = document.createElement('ul');
+    var result = document.getElementById('listResult');
+    result.appendChild(unList);
+    for (var j = 0; j < productArray.length; j++) {
+      var list = document.createElement ('li');
+      list.innerText = productArray[j].votes / productArray[j].displayed * 100 + (' votes for the  ' + productArray[j].name + '.');
+      unList.appendChild(list);
+    }
+  };
 };
-// totalClicks++;
-// var targetId = whichImage.target.getAttribute('productId');
-// for (productArray = 0; productArray < 25; productArray++) {
-//   if #################################
-//     #############################.countClicks++;
-//   }
-// }
-// }
-//
-
-// function clickTally() {
-//   var imageOneSelected = document.getElementById('imageSpotOne');
-//   var imageTwoSelected = document.getElementById('imageSpotTwo');
-//   var imageThreeSelected = document.getElementById('imageSpotThree');
-  // if productX is clicked, productXTally++;
-// };
-
-// row.appendChild()
-// // Count how many times each image is shown
-// when image is clicked
-// count click for that image
-// remove eventlistener
-// reset images
-// repeat
