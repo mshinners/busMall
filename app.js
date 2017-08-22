0baf6510172a073ae2bcee4fefeb2ccf28877bb4
Michael Shinners <mishinners@icloud.com>
2017-08-21 17:01:55 -0700

    3 pics being randomly rendered (properly?)

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-"use strict";
+'use strict';
 // array of all image files
-var productImageArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.jpg', 'water-can.jpg', 'wine-glass.jpg'];
-// iterate through arrsay using a counter
-//
-// for (var i = 0; i < productImageArray.length; i++) {
-//
-// }
-
+// var productImageArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.jpg', 'water-can.jpg', 'wine-glass.jpg'];
 
 //ImageDisplayField Constructor Function
 function Product(productName, imageFilePath, timesImageShown, timesImageClicked) {
@@ -14,7 +8,11 @@ function Product(productName, imageFilePath, timesImageShown, timesImageClicked)
   this.imageFilePath = imageFilePath;
   this.timesImageShown = timesImageShown;
   this.timesImageClicked = timesImageClicked;
+  productArray.push(this);
 }
+
+var productArray = [];
+var lastThreeImgs = [];
 
 var Bag = new Product('bag', 'imgs/bag.jpg', 0, 0);
 var banana = new Product('banana', 'imgs/banana.jpg', 0, 0);
@@ -37,12 +35,31 @@ var usb = new Product('usb', 'imgs/usb.gif', 0, 0);
 var waterCan = new Product('waterCan', 'imgs/water-can.jpg', 0, 0);
 var wineGlass = new Product('wineGlass', 'imgs/wine-glass.jpg', 0, 0);
 
-function renderProduct() {
+function renderThreeProducts() {
   var imageOne = document.getElementById('imageSpotOne');
   var productOne = document.createElement('img');
-  productOne.src = Bag.imageFilePath;
+  var randomOne = Math.floor(Math.random() * productArray.length);
+  productOne.src = productArray[randomOne].imageFilePath;
   imageOne.appendChild(productOne);
+  var imageTwo = document.getElementById('imageSpotTwo');
+  var productTwo = document.createElement('img');
+  productTwo.src = Bag.imageFilePath;
+  imageTwo.appendChild(productTwo);
+  var randomTwo = Math.floor(Math.random() * productArray.length);
+  while (randomOne === randomTwo) {
+    randomTwo = Math.floor(Math.random() * productArray.length);
+  }
+  productTwo.src = productArray[randomTwo].imageFilePath;
+  imageTwo.appendChild(productTwo);
+  var imageThree = document.getElementById('imageSpotThree');
+  var productThree = document.createElement('img');
+  var randomThree = Math.floor(Math.random() * productArray.length);
+  while (randomThree === randomTwo || randomThree === randomOne) {
+    randomThree = Math.floor(Math.random() * productArray.length);
+  }
+  productThree.src = productArray[randomThree].imageFilePath;
+  imageThree.appendChild(productThree);
+  lastThreeImgs = [];
+  lastThreeImgs.push(randomOne, randomTwo, randomThree);
 };
-renderProduct();
-
-//Product();
+renderThreeProducts();
